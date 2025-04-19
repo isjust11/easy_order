0d1607b48c9e6ea23eaf2985ef764ec9bfdd8e84
vovanhung <vovanhung@vnpt.vn>
2025-04-19 10:32:05 +0700

    add font

--- clients/app/layout.tsx
+++ clients/app/layout.tsx
@@ -1,15 +1,16 @@
 
-import { Outfit } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
 import { SidebarProvider } from '@/contexts/SidebarContext';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { Toaster } from 'sonner';
 
-const outfit = Outfit({
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin', 'vietnamese'],
+  weight: ['400', '500','600', '700'],
+  variable: '--font-inter',
 });
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
+      <body className={`${inter.variable} font-sans dark:bg-gray-900`}>
         <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
           <Toaster/>
